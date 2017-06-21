b466ebd40a1d054ac21672df5b32bec978cea098
Grisha Pomadchin <gr.pomadchin@gmail.com>
2017-06-21 17:02:52 +0300

    add comments

--- app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/color/functions/Approximations.scala
+++ app-backend/datamodel/src/main/scala/com/azavea/rf/datamodel/color/functions/Approximations.scala
@@ -1,6 +1,20 @@
 package com.azavea.rf.datamodel.color.functions
 
+/**
+  * Not very good pow and exponent approximations
+  * consider org.apache.commons.math{3|4}.util.FastMath usage
+  * in cases where these functions can't be applied.
+  *
+  * ApproximationsSpec demonstrates max deviation on SaturationAdjust and SigmoidalContrast domains
+  */
 object Approximations {
+
+  /**
+    * For clarifications
+    * see https://www.reddit.com/r/gamedev/comments/n7na0/fast_approximation_to_mathpow/
+    * and https://martin.ankerl.com/2007/10/04/optimized-pow-approximation-for-java-and-c-c/
+    *
+    */
   def pow(a: Double, b: Double): Double = { // exponentiation by squaring
     if(a < 1 && java.lang.Double.isInfinite(b)) return 0d
     if(a >= 1 && java.lang.Double.isInfinite(b)) return Double.NaN
@@ -21,6 +35,11 @@ object Approximations {
     r * java.lang.Double.longBitsToDouble(tmp2)
   }
 
+  /**
+    * For clarifications
+    * see http://martin.ankerl.com/2007/02/11/optimized-exponential-functions-for-java/
+    *
+    */
   def exp(value: Double): Double = {
     val tmp = (1512775 * value + (1072693248 - 60801)).toLong
     java.lang.Double.longBitsToDouble(tmp << 32)
